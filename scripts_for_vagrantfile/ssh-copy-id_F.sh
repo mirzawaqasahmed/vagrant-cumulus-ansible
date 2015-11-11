bdfc0e3bc637761f0b45f525e57fe64ac5aa596f
netvirt <m.w.ahmed@gmail.com>
2015-11-11 11:57:09 +0400

    updated expect script in ssh-copy-id_F.sh

--- scripts_for_vagrantfile/ssh-copy-id_F.sh
+++ scripts_for_vagrantfile/ssh-copy-id_F.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/expect
+#!/usr/bin/expect -c
 set timeout 10
 set f [open "hostsfile"]
 set hosts [split [read -nonewline $f] "\n"]
 close $f
-
 
 foreach host $hosts {
     spawn ssh-copy-id -i /home/vagrant/.ssh/ansible-use-ssh-key.pub $host
@@ -13,6 +12,10 @@ foreach host $hosts {
                     exp_continue
                 }
                 "already exist on the remote system." {
+                    exp_continue
+                }
+                "(yes/no)?" {
+                    send "yes\r"
                     exp_continue
                 }
     eof
